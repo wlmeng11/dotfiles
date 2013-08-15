4caae25dae23bef7b215b92bc17fe6bb8a44f7c3
William Meng <wlmeng11@gmail.com>
2013-08-15 15:54:16 -0700

    update display script

--- home/bin/xdisplay.sh
+++ home/bin/xdisplay.sh
@@ -2,7 +2,12 @@
 
 if xrandr -q | grep "VGA1 connected" > /dev/null ; then
 	xrandr --output LVDS1 --mode 1366x768 --primary --output VGA1 --mode 1920x1080 --above LVDS1
+elif xrandr -q | grep "DP1 connected" > /dev/null ; then
+	xrandr --auto
+	xrandr --output LVDS1 --off
+	xmodmap ~/.Xmodmap
 else
 	xrandr --auto
 	xrandr --output VGA1 --off
+	xrandr --output DP1 --off
 fi
