c5dcf9f6baa3052fba1904ec1f8a52866d5bb7e8
William Meng <wlmeng11@gmail.com>
2013-11-28 11:58:57 -0800

    update display switching script

--- home/bin/xdisplay.sh
+++ home/bin/xdisplay.sh
@@ -5,9 +5,16 @@ if xrandr -q | grep "VGA1 connected" > /dev/null ; then
 elif xrandr -q | grep "DP1 connected" > /dev/null ; then
 	xrandr --auto
 	xrandr --output LVDS1 --off
-	xmodmap ~/.Xmodmap
+	xmodmap ~/.Xmodmap # keymappings
 else
 	xrandr --auto
 	xrandr --output VGA1 --off
 	xrandr --output DP1 --off
 fi
+nitrogen --restore # render wallpaper
+
+# reload i3bar without reloading i3wm
+killall i3bar
+for i in 0 1 2; do
+	i3bar --bar_id=bar-$i
+done
