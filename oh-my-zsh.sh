2c0dfc656e96dfdd9bccfd64f858aa81b7fad564
William Meng <wlmeng11@gmail.com>
2023-08-03 16:49:13 -0700

    Install zsh-syntax-highlighting plugin

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -7,3 +7,6 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 
 ln -sf dotfiles/home/.zshrc ~
 chsh -s `command -v zsh`
+
+# Install syntax highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
